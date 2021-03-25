b0454400ce2988f71321427fd4c8e9d89b0581ee
catrix <antonin.catrix@epsi.fr>
2021-03-25 11:27:34 +0100

    finish project for Luc

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import service.StudentService;
